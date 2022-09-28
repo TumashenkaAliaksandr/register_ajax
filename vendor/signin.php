64f8279bb236c7f1818a8e60032db9158d3d6883
Python Developer <78371670+TumashenkaAliaksandr@users.noreply.github.com>
2022-09-28 18:04:07 +0300

    Update signin.php

--- vendor/signin.php
+++ vendor/signin.php
@@ -34,9 +34,6 @@ $password = md5($password);
 $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
 if (mysqli_num_rows($check_user) > 0) {
 
-    $jsonArray[] = $check_user;
-    file_put_contents('username.json', json_encode($jsonArray, JSON_FORCE_OBJECT));
-
     $user = mysqli_fetch_assoc($check_user);
 
     $_SESSION['user'] = [
